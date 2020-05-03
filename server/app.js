501cc4a9c3b7a9299811dd9606a335dfdf690cfe
Danna <wdanna_@outlook.com>
2020-05-03 20:31:19 +0800

    ADD compose file for production env

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-
+const {pool} = require('./config');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
@@ -9,6 +9,17 @@ const app = express();
 app.get('/api/hello-world', (req, res) => {
   res.send('Hello World');
 });
+
+const getBooks = (req, res) => {
+  pool.query('SELECT * FROM books', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+}
+
+app.route('/api/postgres').get(getBooks)
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
